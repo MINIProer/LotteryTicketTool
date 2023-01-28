25b6a9f102480ba5566872cef3c9ebaad8f5319a
jiangshulun <jiangshulun@joyme.sg>
2023-01-28 14:22:10 +0800

    初始化空项目

---
+++ LotteryTicket/LotteryTicket/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  LotteryTicket
+//
+//  Created by jiangshulun on 2023/1/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
